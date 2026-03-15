c5486d46235f1d33612b50127eb25de0950c0817
Marco <86566346+marcoperozzo@users.noreply.github.com>
2026-03-15 21:58:16 +0000

    requests

--- 01.streamlit_app.py
+++ 01.streamlit_app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Create Your Smoothie! :cup_with_straw: ")
@@ -24,6 +25,11 @@ ingredients_list = st.multiselect(
 ,   my_dataframe
 ,   max_selections=5
 )
+
+# New section to display smoothiefroot nutrition information
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # print the selected itens
 if ingredients_list:
